158174d20b535d171058f70fe409763c91a8bd5e
Travis Flatt <travisflatt@traviss-air.attlocal.net>
2015-07-07 22:29:01 -0500

    Initial commit

--- SpotMark/CurrentLocationViewController.swift
+++ SpotMark/CurrentLocationViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  CurrentLocationViewController.swift
 //  SpotMark
 //
 //  Created by Travis Flatt on 7/7/15.
@@ -8,7 +8,18 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class CurrentLocationViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UILabel!
+    @IBOutlet weak var getButton: UILabel!
+    
+    @IBAction func getLocation() {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
